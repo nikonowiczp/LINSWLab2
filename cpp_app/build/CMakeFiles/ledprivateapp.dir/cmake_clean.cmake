930ffee7eb7a99c103c250b4abda369caa8b9b27
Patryk Nikonowicz <pk.nikonowicz@gmail.com>
2022-04-22 01:04:09 +0200

    Initial commit

---
+++ cpp_app/build/CMakeFiles/ledprivateapp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ledprivateapp.dir/main.cpp.o"
+  "CMakeFiles/ledprivateapp.dir/main.cpp.o.d"
+  "ledprivateapp"
+  "ledprivateapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ledprivateapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
